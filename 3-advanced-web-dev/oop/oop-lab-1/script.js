dc8e01dd38b97cadafdf7b59ec428852ee86af46
Blake Sizemore <blake.sizemore@outlook.com>
2023-08-07 00:09:24 -0400

    push for OOP lab

--- 3-advanced-web-dev/oop/oop-lab-1/script.js
+++ 3-advanced-web-dev/oop/oop-lab-1/script.js
@@ -49,45 +49,74 @@ $(document).ready(function () {
     //     p9.sayHey();
     //     p10.sayHey();
 
-    class vehicle {
+    class automobile {
         constructor(type, maker, wheels) {
             this.type = type;
             this.maker = maker;
             this.wheels = wheels;
         };
-        aboutVehicle() {
-            console.log(`This vechile is made by ${this.maker}, is a ${this.type} and has ${this.wheels} wheels`);
-        }
-    }
 
-    class truck extends vehicle {
+
+        aboutAuto() {
+            let script = `This automobile is a ${this.type} made by ${this.maker} which has ${this.wheels} wheels.`
+            console.log(script);
+        };
+    };
+
+
+
+    class truck extends automobile {
         constructor(type, maker, wheels, doors, bed) {
             super(type, maker, wheels);
             this.doors = doors;
             this.bed = bed;
         }
 
-        aboutVehicle() {
-            console.log(`This car is made by ${this.maker}, is a ${this.type}, has ${this.wheels} wheels,${this.doors} doors,${this.bed}.`);
+        aboutAuto() {
+            let script = `This automobile is a ${this.type} made by ${this.maker} which has ${this.wheels} wheels`;
+            console.log(this);
+            console.log(script);
+
+            if (this.bed == undefined) {
+                console.log(script.concat(`, and has ${this.doors} doors`));
+                console.log(this);
+            } else {
+                console.log(script.concat(` plus a truck bed.`))
+                console.log(this)
+            }
+            console.log(this);
+        };
+    }
+
+    class sedan extends automobile {
+        constructor(type, maker, wheels, doors, size, mpg) {
+            super(type, maker, wheels, doors);
+            this.doors = doors;
+            this.size = size;
+            this.mpg = mpg;
+        }
+
+        aboutAuto() {
+            let mpg = [`30 mpg`, `20 mpg`];
+            script = script.concat(`, and has ${this.doors} doors`);
+            if (this.size == `small`) {
+                console.log(script.concat(` and gets ${mpg[1]}.`));
+            } else if (this.size == `medium`) {
+                console.log(script.concat(` and gets ${mpg[0]}.`));
+            }
         }
     }
 
-
-    // }
-    // class sedan extends vehicle {
-    //     constructor(type, maker, wheels, doors, size, mpg) {
-    //         super(type, maker, wheels, doors);
-    //         this.doors = doors;
-    //         this.size = size;
-    //         if (this.size = `small`){
-    //             this.mpg = 30+`mpg`
-    //         } else if (this.size = `medium`){
-    //             this.mpg = 20+`mpg`
-    //         }else if (this.size = `large`){
-    //             this.mpg = 15+`mpg`
-    //         };
-    //     }
-    // }
+    let v0 = new automobile(`Car`, `Toyota`, 2, 2);
+    v0.aboutAuto();
+    let v1 = new truck(`Truck`, `Nissan`, 2, 2);
+    v1.aboutAuto();
+    let v2 = new truck(`Truck`, `Nissan`, 2, 2, `bed`);
+    v2.aboutAuto();
+    // let v3 = new sedan(`Sedan`, `BMW`, 4, 4, `small`);
+    // v3.aboutAuto();
+    // let v4 = new sedan(`Sedan`, `BMW`, 4, 4, `medium`);
+    // v4.aboutAuto();
     // class motorcyles extends vehicle {
     //     constructor(type, maker, wheels, doors, steering){
     //         super(type, maker, wheels, doors)
@@ -96,15 +125,15 @@ $(document).ready(function () {
     //     }
     // }
 
-    let v0 = new vehicle(`Car`, `Toyota`, 2);
+    // let v0 = new vehicle(`Car`, `Toyota`, 2);
     // let v1 = new sedan(`sedan`,`Nissain`, 4,4,`medium`,);
     // let v2 = new sedan(`sedan`,`Honda`, 4,`small`);
     // let v3 = new sedan(`sedan`,`Ford`, 4,`small`);
-    let v4 = new truck(`Truck`, `nissain`, 4, 4, `and a bed`);
+    // let v4 = new truck(`Truck`, `nissan`, 4, 4);
 
 
-    v0.aboutVehicle();
-    v4.aboutVehicle();
+    // v0.aboutVehicle();
+    // s
 
     // class basic {
     //     constructor(){
