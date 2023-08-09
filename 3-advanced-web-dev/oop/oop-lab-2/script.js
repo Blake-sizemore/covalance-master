b4258eff1fa7945659bf6d755412926c315a0f96
Blake Sizemore <blake.sizemore@outlook.com>
2023-08-08 20:17:33 -0400

    oop-lab-update

--- 3-advanced-web-dev/oop/oop-lab-2/script.js
+++ 3-advanced-web-dev/oop/oop-lab-2/script.js
@@ -1,25 +1,43 @@
 $(document).ready(function () {
-    $(`body`).addClass(`contianer`);
-    $(`header`).addClass(`row justify-content-center`);
-    $(`header`).append(`<button>`);
-    $(`button`).text(`click me to roll a die`);
-    $(`button`).addClass(`col col-sm-3 btn bg-primary`);
-    $('main').addClass(`row bg-dark`);
-    $(`main`).append(`<div>`);
-    $(``).text(textString);
-    $(`div`).addClass(`row`);
-    class die {
-        constructor(){
-            this.div = $(`main`).append(`<div>`);
-            $(this.div).addClass(`row bg-dark`);
+    $(`body`).addClass(`contianer`).addClass(`bg-dark`);
+    $(`header`).addClass(`row justify-content-center`).append(`<button>`);
+    $(`button`).text(`Generate Die`).addClass(`col col-4 btn bg-primary m-1`);
+    $('main').addClass(`row bg-dark justify-content-center my-3`).append(`<div>`);
+    $(`main div`).addClass(`dieHold row justify-content-center`);
+    let button = document.querySelector(`button`);
+    let dieCount = 0;
+
+    class Die {
+        constructor() {
+            this.div = document.createElement(`div`);
             this.value = this.roll();
+            $(this.div).addClass(`die bg-primary text-center borader boarder rounded-2 m-1`).text(`${this.value}`).width(`100px`).height(`100px`).attr(`id`,`${dieCount}`);
+            console.log(this);
+            $(this.div).appendTo(`div.dieHold`);
+            this.reRoll();
+        }
+        roll() {
+            return Math.floor(Math.random() * 6);
+        }
+        reRoll(){
+            this.die = document.getElementsByClassName(`die`);
+            if (dieCount == 0) {
+                $(`main`).append(`<button>`);
+                $(`button.roll`).addClass(`row btn bg-primary text-center borader boarder rounded-2 m-1 roll`).text(`reroll all dice`);
+            }
 
         }
-        roll(){
-            return Math.floor(Math.random()*6);
-        }
     }
-    new die();
+
+    class roll extends Die {
+
+    }
+    button.addEventListener('click', function () { 
+        new Die(); 
+        ++dieCount;
+        
+    }
+        );
     // button.addEventListener('click', function () {
     // })
 
