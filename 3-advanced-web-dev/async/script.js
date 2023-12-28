c9ad708e1c4f5e7a5adb227c58a53faaf81be4d7
Blake Sizemore <blake.sizemore@outlook.com>
2023-12-27 20:42:31 -0500

    update git for pc

--- 3-advanced-web-dev/async/script.js
+++ 3-advanced-web-dev/async/script.js
@@ -4,20 +4,6 @@ $(document).ready(function () {
         return console.log(instant);
     }
 
-    instantReturn(`This callback is instantReturn line 7, function declared on 3 through 5 and got logged instantly`);
-
-    let instantReturnTwo = (instant) => {
-        return console.log(instant);
-    }
-
-    instantReturnTwo(`This callback is instantReturnTwo line 12, function declared on 9 through 11 and got logged instantly`);
-
-    // let getWords(message, delay){
-    //     instantReturn(``This callback is instantReturn line 7, function declared on 3 through 5 and got logged instantly``)
-
-    //     })
-    // }
-
     function echo(message, delay) {
         return new Promise(function (resolve, reject) {
             setTimeout(() => {
@@ -28,42 +14,52 @@ $(document).ready(function () {
         });
     };
 
-    echo(`hello 3000`,3000)
-        .then((a)=>{
-            console.log(a);
-            echo(`hello again 2000`,2000)
-                .then((a)=>{
-                    console.log(a);
-                    echo(`hello again 1000`,1000)
-                        .then((a)=>{
+    let getWords = () => {
+        instantReturn(`This callback is instantReturn line 7, function declared on 3 through 5 and got logged instantly`);
+        echo(`hello 3000`, 3000)
+            .then((a) => {
+                console.log(a);
+                echo(`hello again 2000`, 2000)
+                    .then((a) => {
                         console.log(a);
-                })
-        }).catch((e)=>{
-            console.log(`An error ocurred!`);
-            console.log(e);
+                        echo(`hello again 1000`, 1000)
+                            .then((a) => {
+                                console.log(a);
+                            })
+                    }).catch((e) => {
+                        console.log(`An error ocurred!`);
+                        console.log(e);
+                    });
+            })
+    }
+
+    getWords();
+
+    // Create a function called done that logs Job's done! to the console
+    // Create a different function (on the global scope) called countdown that accepts two arguments, num and callback
+    //     The goal of this function will be to log a "countdown" from the number passed in as an argument, delayed by 1 second each time, and when it gets to 1, it should execute the callback argument
+
+    function done(){
+        console.log(`Job's Done`);
+    }
+
+    function countdown (num,callback){
+        setTimeout(()=>{
+            console.log(num),1000;
         });
+        const newNum = num - 1;
 
-    // document.getElementById(btn).addEventListener(`click`,()=>{
-    //     slowEcho(`Hello`,1000)
-    //     .then((a)=>{
-    //         console.log(a);
-    //     }).catch((e)=>{
-    //         console.log(`An error ocurred!`);
-    //         console.log(e);
-    //     });
-    // });
+        let promise = new Promise ( function (resolve,reject){
+            if (num == 0){
+                resolve(done());
+            }else {
+                countDown(newNum,callback);
+            }
+            // let err = new Error (`oops something went wrong`);
+            // reject(err);
+        })
+    }
 
-    // let resolveAfter2Seconds = () => {
-    //     console.log('starting slow promise');
-
-    //     return new Promise(resolve => {
-    //         setTimeout(() => {
-    //             resolve(20);
-    //             console.log('slow promise is done');
-    //         }, 2000);
-    //     });
-    // };
-
-});
+countdown(10,);
 
 });
